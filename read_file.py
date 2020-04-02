243bd29e3d730fa0e26cb5acf641d909810c6afb
Z4HRA <zahrasarlak12@gmail.com>
2020-04-02 20:35:55 +0430

    add split func

---
+++ read_file.py
@@ -0,0 +1,20 @@
+from scipy.io import loadmat
+import numpy as np
+
+
+def to_numpy(data):
+    features = data["fts"]
+    labels = data["labels"]
+    data_matrix = np.concatenate((features, labels), axis=1)
+    return data_matrix
+
+
+def read(path):
+    data = loadmat(path)
+    data = to_numpy(data)
+    return data
+
+
+def split(data, k=5):
+    np.random.shuffle(data)
+    return np.array_split(data, k)
