fix convergence condition in gradient descent

gradient_descent.py
@@ -2,18 +2,21 @@ import numpy as np
 import time
 
 
-def gradient_descent(gradient_func, number_of_classes, number_of_features,
-                     step=0.001, gradient_precision=0.0001):
-    coef = np.ones((number_of_classes, number_of_features))
-    gradient = 1
-    iteration = 0
+def gradient_descent(gradient_func, coef_shape, iteration=1000, step=0.001):
+    coef = np.ones(coef_shape)
+    gradient = np.ones(coef_shape)
+    gr_diff = 0.01
+    iter = 0
     tik = time.time()
-    #while np.linalg.norm(gradient) > gradient_precision:
-    for i in range(10000):
-        iteration += 1
+    for i in range(iteration):
+        iter += 1
+        prev = gradient
         gradient = gradient_func(coef)
         coef = coef - step * gradient
+        if np.max(np.abs(gradient - prev)) < gr_diff:
+            break
+
     tok = time.time()
-    print("We reached gradient:", np.linalg.norm(gradient), "in ", str(iteration), "iteration", "and in",
+    print("We reached gradient:", gradient, "in ", str(iter), "iteration", "and in",
           tok - tik, "seconds")
     return coef