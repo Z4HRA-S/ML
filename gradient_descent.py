055728f92d63f8791c7f79e56a5d5207010cee28
Z4HRA <zahrasarlak12@gmail.com>
2020-04-09 19:11:46 +0430

    add initial point parameter

--- gradient_descent.py
+++ gradient_descent.py
@@ -2,8 +2,8 @@ import numpy as np
 import time
 
 
-def gradient_descent(gradient_func, coef_shape, iteration=1000, step=0.001):
-    coef = np.ones(coef_shape)
+def gradient_descent(gradient_func, coef_shape, initial=1, iteration=1000, step=0.001, mute=False):
+    coef = initial * np.ones(coef_shape)
     gradient = np.ones(coef_shape)
     gr_diff = 0.1
     iter = 0
@@ -16,6 +16,8 @@ def gradient_descent(gradient_func, coef_shape, iteration=1000, step=0.001):
         if np.max(np.abs(gradient)) < gr_diff:
             break
     tok = time.time()
-    print("We reached gradient:", np.linalg.norm(gradient), "with step:", step, "in ", str(iter), "iteration", "and in",
-          tok - tik, "seconds")
+    if not mute:
+        print("We reached gradient:", np.linalg.norm(gradient), "with step:", step, "in ", str(iter), "iteration",
+              "and in",
+              tok - tik, "seconds")
     return coef
